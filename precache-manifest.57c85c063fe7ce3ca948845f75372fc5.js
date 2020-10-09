e82efc3f2067edd1eed11428a3471bd962e410eb
Dawid Cichy <dawid.cichy@gft.com>
2020-10-09 22:44:44 +0200

    Updates

---
+++ precache-manifest.57c85c063fe7ce3ca948845f75372fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd683088fdc732f29d71b99f427a3f09",
+    "url": "/product/index.html"
+  },
+  {
+    "revision": "b6f31f74699c136882cf",
+    "url": "/product/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94c93b6f339ff68ed55a",
+    "url": "/product/static/js/2.11b785af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/product/static/js/2.11b785af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f31f74699c136882cf",
+    "url": "/product/static/js/main.5a00e6c0.chunk.js"
+  },
+  {
+    "revision": "40dbdb163db4f3941129",
+    "url": "/product/static/js/runtime-main.7437e29e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/product/static/media/logo.5d5d9eef.svg"
+  }
+]);
